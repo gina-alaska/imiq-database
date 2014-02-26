07f32557fbda1927fe21d513aeb654fa61fb6409
Amy Jacobs <asjacobs@alaska.edu>
2014-02-26 09:58:27 -0900

    datastreams
    Revised update query for ‘Datastreams’ table, updates the begin and end
    date of the datastream data values.  Updated to work with datastreams
    that have no data values added

--- sql/update/update_datastreams_adddates.sql
+++ sql/update/update_datastreams_adddates.sql
@@ -9,14 +9,15 @@ WHILE @@FETCH_STATUS = 0
 BEGIN
   select @BeginDateTime = LTRIM(STR(YEAR(MIN(v.LocalDateTime))))+'-'+ RIGHT('0' + CAST(MONTH(MIN(v.LocalDateTime)) AS VARCHAR), 2) +'-'+RIGHT('0' + CAST(DAY(MIN(v.LocalDateTime)) AS VARCHAR), 2), 
          @EndDateTime= LTRIM(STR(YEAR(MAX(v.LocalDateTime))))+'-'+ RIGHT('0' + CAST(MONTH(MAX(v.LocalDateTime)) AS VARCHAR), 2) +'-'+RIGHT('0' + CAST(DAY(MAX(v.LocalDateTime)) AS VARCHAR), 2)
-  from Datastreams d
-  inner join DataValues v on v.DatastreamID=d.DatastreamID
+  from DataValues v
+  where v.DatastreamID=@datastreamID
   GROUP BY v.DatastreamID,UTCOffset
 
  update Datastreams
   set BDATE=@BeginDateTime,
       EDATE=@EndDateTime
   where datastreamid=@datastreamID 
+ select @BeginDateTime = NULL, @EndDateTime=NULL
 FETCH NEXT FROM loop_cursor INTO @datastreamID;
 END;
 CLOSE loop_cursor;
