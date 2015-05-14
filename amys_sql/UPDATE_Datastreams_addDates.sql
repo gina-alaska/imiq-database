e424e72647cdc2b713c5047c4b61914a26d280bb
asjacobs <asjacobs@alaska.edu>
2015-05-14 09:47:15 -0800

    updated datasets, psql and processing notes
    psql \copy commands used to load data into
    ‘imiq_staging’, SQL and a few scripts

---
+++ amys_sql/UPDATE_Datastreams_addDates.sql
@@ -0,0 +1,29 @@
+﻿--Update tables.datastreams to add the BDATE and EDATE for the datastreams.
+DO $$
+DECLARE
+BeginDateTime VARCHAR(10);
+EndDateTime VARCHAR(10);
+datastream_ID INTEGER;
+loop_cursor refcursor;
+
+BEGIN
+
+OPEN loop_cursor FOR execute('select datastreamid from tables.datastreams');
+loop
+  fetch loop_cursor into datastream_ID;
+  if not found then
+	exit;
+  end if;
+  select to_char(min(v.localdatetime),'YYYY-MM-DD'), to_char(max(v.localdatetime),'YYYY-MM-DD') into BeginDateTime, EndDateTime
+  from tables.datavalues v
+  where v.datastreamid = datastream_ID
+  GROUP BY v.datastreamid,v.utcoffset;
+
+ update tables.datastreams
+  set BDATE = BeginDateTime,
+      EDATE = EndDateTime
+  where datastreamid = datastream_ID; 
+ select NULL,NULL into BeginDateTime, EndDateTime;
+ end loop;
+close loop_cursor;
+END $$;
