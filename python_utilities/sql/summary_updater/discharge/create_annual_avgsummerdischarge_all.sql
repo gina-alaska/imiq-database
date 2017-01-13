bb8317ee57cb46f0c6cf1d70d3765d9395059cb4
rwspicer <rwspicer@alaska.edu>
2017-01-13 13:54:37 -0900

    added discharge files

---
+++ python_utilities/sql/summary_updater/discharge/create_annual_avgsummerdischarge_all.sql
@@ -0,0 +1,27 @@
+﻿-- View: tables.annual_avgsummerdischarge_all
+
+-- DROP VIEW tables.annual_avgsummerdischarge_all;
+
+CREATE TABLE tables.annual_avgsummerdischarge_all_2 AS 
+ SELECT monthly_discharge_all.siteid,
+    monthly_discharge_all.year,
+    avg(monthly_discharge_all.monthlyavg) AS seasonalavg
+   FROM tables.monthly_discharge_all
+  WHERE monthly_discharge_all.month >= 6::double precision AND monthly_discharge_all.month <= 8::double precision AND monthly_discharge_all.monthlyavg IS NOT NULL
+  GROUP BY monthly_discharge_all.siteid, monthly_discharge_all.year
+ HAVING count(*) = 3;
+ 
+CREATE INDEX annual_avgsummerdischarge_all_idx
+  ON tables.annual_avgsummerdischarge_all_2
+  USING btree
+  (siteid);
+  
+ALTER TABLE tables.annual_avgsummerdischarge_all_2
+  OWNER TO imiq;
+GRANT ALL ON TABLE tables.annual_avgsummerdischarge_all_2 TO imiq;
+GRANT ALL ON TABLE tables.annual_avgsummerdischarge_all_2 TO asjacobs;
+GRANT ALL ON TABLE tables.annual_avgsummerdischarge_all_2 TO chaase;
+GRANT SELECT ON TABLE tables.annual_avgsummerdischarge_all_2 TO imiq_reader;
+GRANT ALL ON TABLE tables.annual_avgsummerdischarge_all_2 TO rwspicer;
+COMMENT ON TABLE tables.annual_avgsummerdischarge_all_2
+  IS 'This view creates annual average summer discharge using "monthly_discharge_all".  Requires all three months; June, July, August; to create annual average summer discharge.';
---
+++ python_utilities/sql/summary_updater/discharge/create_annual_peakdischarge.sql
@@ -0,0 +1,30 @@
+﻿-- View: tables.annual_peakdischarge
+
+-- DROP VIEW tables.annual_peakdischarge;
+
+CREATE TABLE tables.annual_peakdischarge_2 AS 
+ SELECT row_number() OVER (ORDER BY annual_peakdischarge_all.siteid, annual_peakdischarge_all.year) AS valueid,
+    annual_peakdischarge_all.datavalue,
+    annual_peakdischarge_all.siteid,
+    (annual_peakdischarge_all.year || '-01-01'::text)::timestamp without time zone AS utcdatetime,
+    689 AS originalvariableid,
+    712 AS variableid
+   FROM tables.annual_peakdischarge_all;
+
+ALTER TABLE tables.annual_peakdischarge_2
+  ADD CONSTRAINT annual_peakdischarge_valueid PRIMARY KEY (valueid);
+
+CREATE INDEX annual_peakdischarge_siteid_idx
+  ON tables.annual_peakdischarge_2
+  USING btree
+  (siteid);
+
+ALTER TABLE tables.annual_peakdischarge_2
+  OWNER TO imiq;
+GRANT ALL ON TABLE tables.annual_peakdischarge_2 TO imiq;
+GRANT ALL ON TABLE tables.annual_peakdischarge_2 TO asjacobs;
+GRANT ALL ON TABLE tables.annual_peakdischarge_2 TO chaase;
+GRANT SELECT ON TABLE tables.annual_peakdischarge_2 TO imiq_reader;
+GRANT ALL ON TABLE tables.annual_peakdischarge_2 TO rwspicer;
+COMMENT ON TABLE tables.annual_peakdischarge_2
+  IS 'This view creates "annual_peakdischarge" with the fields: valueid, datavalue, siteid, utcdatetime, originalvariableid and variableid.  Sets originalvariableid=689 and variableid=712';
