9eb49ef93aeeb99040a1d40d4caede62f23d4ac1
rwspicer <rwspicer@alaska.edu>
2017-01-27 16:13:08 -0900

    added special conditions, fetch dsdi, scalers, replace to y2i

--- python_utilities/Y2I/y2i.py
+++ python_utilities/Y2I/y2i.py
@@ -12,6 +12,7 @@ from pandas import read_csv,concat,DataFrame,to_datetime
 import yaml
 import os
 import sys
+import warnings
 
 class Y2I (object):
     """Yaml to Imiq (or Insert). Use to convert csv files to instert scripts 
@@ -54,6 +55,7 @@ class Y2I (object):
                     #~ print template_args[arg]
                     text = text.replace('<' + arg + '>',
                         str(template_args[arg]))
+                #~ print text
                 self.config = yaml.load(text)
                 #~ print text
         
@@ -80,12 +82,19 @@ class Y2I (object):
     def load_infiles (self):
         """load the input files. Sets data[fame] = <file_data> for each file
         """
-        #~ print self.config
+        #~ print self.config.keys()
+        #~ print self.config['infiles']
         for f in self.config['infiles']:
             #~ print f
             self.data[f] = read_csv(f, 
                             skiprows = self.config['infiles'][f]['skiprows'])
-
+            self.data[f] = self.data[f][self.config['infiles'][f]['columns']]
+            
+            try:
+                self.data[f] = self.data[f][self.data[f][self.config['infiles'][f]['skipnan']].isnull() == False]
+            except KeyError:
+                pass
+                
     def load_length (self):
         """load the lenth of the data to add into imiq. Sets length"""
         if 'of_file' in self.config['length'].keys():
@@ -100,7 +109,13 @@ class Y2I (object):
         
         for col in self.config['columns']:
             self.columns.append(col['name'])
-            if col['source'] in self.data.keys():
+            
+            if col['source'] == "imiq":
+                siteid = get_datastreamid(col['sitecode'],col['variablecode'],col['imiq_login'])
+                #~ print siteid
+                self.table[col['name']] = siteid
+            
+            elif col['source'] in self.data.keys():
                 try:
                     if type(col['value']) is int:
                         col['value'] = self.data[col['source']].columns[col['value']]
@@ -121,8 +136,29 @@ class Y2I (object):
                         self.data[col['source']][col['elev']].astype(str) + ')'
                 else:
                     dt = col['dtype']
-                    values = self.data[col['source']][col['value']].astype(dt)
-                
+                    values = self.data[col['source']][col['value']]
+                    try:
+                        if not col['replace'] is None:
+                            warnings.filterwarnings("ignore")
+                            #~ for col['replace'].keys():
+                            reps = list( set(col['replace'].keys()) -\
+                                            set(['default']))
+                            values[values.isin(reps) == False] = \
+                                                col['replace']['default']
+                            for k in reps:
+                                values[values == k] = col['replace'][k]
+                    except KeyError as e:
+                        pass
+                    warnings.filterwarnings("default")
+                    
+                    values = values.astype(dt)
+                    # apply scalers
+                    try:
+                        if not col['scaler'] is None:
+                            values[values != -9999] *= col['scaler']
+                    except KeyError as e:
+                        pass
+                    
                 self.table = concat([self.table, values],axis = 1)
             else:
                 #~ print 'b'
@@ -133,6 +169,28 @@ class Y2I (object):
                     self.table[col['name']] = values
                 else:
                     self.table[col['name']] = col['value']
+               
+            #~ self.table
+            
+        self.table.columns = self.columns
+        #~ print self.table
+        #~ print self.table[self.table['PRCP'] == -9999]['PRCP_qflag']
+        warnings.filterwarnings("ignore")
+        try:
+            conditions = self.config['special conditions']
+            for c in conditions:
+                #~ print c['condition'].keys()[0],c['condition'][c['condition'].keys()[0]]
+                #~ print self.table[c['condition'].keys()[0]]
+                idx = self.table[c['condition'].keys()[0]] == c['condition'][c['condition'].keys()[0]]
+                for k in c['result']:
+                    #~ print c['result'][k]
+                    self.table[k][idx] = float(c['result'][k])
+                    #~ print self.table[idx][k]
+        except:
+            pass
+        warnings.filterwarnings("default")
+        
+        #~ print self.table
         #~ print self.columns
     
     def to_string (self):
@@ -188,6 +246,24 @@ class Y2I (object):
         self.create_table()
         self.save_sql()
         
+import posthaste as ph
+def get_datastreamid(sitecode, varcode, creds):
+    s = ph.PostHaste(*ph.load_login(creds))
+    s.sql = """
+select datastreamid from tables.datastreams 
+    where 
+    siteid in (select siteid from tables.sites where sitecode = 'SITECODE') 
+        and 
+    variableid in (select variableid from tables.variables where variablecode = 'VARCODE')
+    """.replace('VARCODE',str(varcode)).replace('SITECODE',str(sitecode))
+
+    s.run()
+
+    try:
+        return s.data().values[0][0]
+    except IndexError:
+        raise StandardError, "Datastreamid not found for: " + str(sitecode) + ' & ' + str(varcode)
+        
 
 def test():
     """test utility
@@ -215,7 +291,12 @@ def main ():
             #~ print  setup['arguments'][i]['value_column']
             converter = Y2I(setup['template'], setup['arguments'][i])
             #~ print converter.config
-            converter.generate_sql()
+            try:
+                converter.generate_sql()
+            except StandardError as e:
+                print e
+                print " -- no sql generated"
+                continue
     except KeyError as e:
         print e
         converter = Y2I(sys.argv[1])
