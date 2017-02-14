2f517e7e5c4d8a326b0046aa671a6e5e085942ed
rwspicer <rwspicer@alaska.edu>
2017-02-14 09:37:15 -0900

    some code cleanup, added if statments for optional config sections

--- python_utilities/Y2I/y2i.py
+++ python_utilities/Y2I/y2i.py
@@ -5,7 +5,10 @@ Y2I (YAML to Insert)
 utility for using yaml file to describe conversion of csv files to .sql 
 insert scripts
 
-V1.0.0
+V1.0.1
+
+change log:
+    V1.0.1: (2017/02/14) updated comments
 
 """
 from pandas import read_csv,concat,DataFrame,to_datetime
@@ -34,15 +37,19 @@ class Y2I (object):
         dictionary of data file keyed by filename
     columns : list
         list of columns to add to insert script
-    length:
-    table
-    tab
+    length: int
+        # of rows in table to insert
+    table : pandas.DataFrame
+        representation of tables data
+    tab: str
+        indetation size for to_string function defaults to 4 spaces
     
     
     """
     
     def __init__ (self, config_file, template_args = None):
-        """ Class initialiser """
+        """Class initialiser 
+        """
         #~ print config_file
         
         if template_args is None:
@@ -92,26 +99,27 @@ class Y2I (object):
         return True, ""
         
     def load_infiles (self):
-        """load the input files. Sets data[fame] = <file_data> for each file
+        """Load the input files. Sets data[fame] = <file_data> for each file.
         """
-        #~ print self.config.keys()
-        #~ print self.config['infiles']
         for f in self.config['infiles']:
-            #~ print f
             self.data[f] = read_csv(f, 
-                            skiprows = self.config['infiles'][f]['skiprows'],
-                             low_memory= False)
+                skiprows = self.config['infiles'][f]['skiprows'],
+                low_memory= False)
                
-            
             try:
-                self.data[f] = self.data[f][self.config['infiles'][f]['columns']]
+                if 'columns' in self.config['infiles'][f].keys():
+                    self.data[f] = \
+                        self.data[f][self.config['infiles'][f]['columns']]
             except KeyError:
-                raise RuntimeError, "cannot find columns " + str(self.config['infiles'][f]['columns'])
+                msg = "cannot find columns " + \
+                    str(self.config['infiles'][f]['columns'])
+                raise RuntimeError, msg
                 
             try:
                 #### NOTE: create a tempfile to make sure rows are droped
                 #### probably possible as strings"
-                self.data[f] = self.data[f].dropna(subset= [self.config['infiles'][f]['skipnan']])
+                self.data[f] = self.data[f].dropna(\
+                    subset = [self.config['infiles'][f]['skipnan']])
                 self.data[f].to_csv('.temp.csv', index = False)
                 try:
                     self.data[f] = read_csv('.temp.csv')
@@ -127,47 +135,64 @@ class Y2I (object):
             #~ print self.data[f].iloc[:10]
                 
     def load_length (self):
-        """load the lenth of the data to add into imiq. Sets length"""
+        """Load the lenth of the data to add into imiq. Sets length."""
         if 'of_file' in self.config['length'].keys():
             self.length = len(self.data[self.config['length']['of_file']])
         else: 
             self.length = self.config['length']['constant']
 
     def create_table (self):
-        """create a proxy the table for the database as a dataframe. sets table.
+        """create a proxy the table for the database as a dataframe. Sets table.
         """
+        ## set up rows for table
         self.table = DataFrame(index = range(self.length))
         #~ print self.table
+        
+        ## set up each column
         for col in self.config['columns']:
-            #~ print col
+        
+            ## add column to the list of columns
             self.columns.append(col['name'])
             
+            ## check each source
             if col['source'] == "imiq":
+                ## Source is on imiq, so get info from database
                 # TODO: add statement for choosing functions.
                 siteid = get_datastreamid(col['sitecode'],
                                           col['variablecode'],
                                           col['imiq_login'])
-                #~ print siteid
                 self.table[col['name']] = siteid
             
             elif col['source'] in self.data.keys():
+                ## source is a data file
+                
+                # TODO: this bit seems redundant ----------------
                 try:
+                    #int data
                     if type(col['value']) is int:
-                        col['value'] = self.data[col['source']].columns[col['value']]
+                        col['value'] = \
+                            self.data[col['source']].columns[col['value']]
                 except KeyError as e:
                     if col['dtype'] == 'geolocation':
                         pass
                     else:
                         raise KeyError, e
+                # -------------------------
+                        
                 if col['dtype'] == 'datetime':
-                    #~ print self.data[col['source']][col['value']]
+                    ## timestamp column, try to infer format, 
+                    ## if not possible then use specified format
                     try:
-                        values = to_datetime(self.data[col['source']][col['value']],
-                                                    infer_datetime_format=True)
+                        values = \
+                            to_datetime(self.data[col['source']][col['value']],
+                            infer_datetime_format=True)
                     except ValueError:
-                        values = to_datetime(self.data[col['source']][col['value']],format = col['format'])
+                        values = \
+                            to_datetime(self.data[col['source']][col['value']],
+                            format = col['format'])
                     
                 elif col['dtype'] == 'geolocation':
+                    ## make geolocation form multiple columns
                     values = "POINT("  + \
                         self.data[col['source']][col['long']].astype(str) + \
                         ' ' + \
@@ -175,8 +200,12 @@ class Y2I (object):
                         ' ' + \
                         self.data[col['source']][col['elev']].astype(str) + ')'
                 else:
+                    ## use data type specified in yaml
                     dt = col['dtype']
                     values = self.data[col['source']][col['value']]
+                    
+                    ## REPLACE feature:
+                    # TODO: remove, use special conditions instead -------------
                     try:
                         if not col['replace'] is None:
                             warnings.filterwarnings("ignore")
@@ -190,62 +219,57 @@ class Y2I (object):
                     except KeyError as e:
                         pass
                     warnings.filterwarnings("default")
+                    #-----------------------------------------------------------
                     
+                    ## Xonvert ot type
                     values = values.astype(dt)
-                    # apply scalers
+                    ## Cpply scalers
                     try:
                         if not col['scaler'] is None and \
                             not type(col['scaler']) is str:
                             values[values != -9999] *= col['scaler']
                     except KeyError as e:
                         pass
-                    
+                ## Add column to table
                 self.table = concat([self.table, values],axis = 1)
             else:
-                #~ print 'b'
-                #~ print col['name']
+                ## source is config file
+                ## TODO: add features that from file has
                 if col['dtype'] == 'datetime':
-                    #~ print 
+                    ## date time type
                     values = to_datetime(col['value'])
                     self.table[col['name']] = values
                 else:
+                    # scaler, or string
                     self.table[col['name']] = col['value']
                
-            #~ self.table
-            
+               
+        ## update Table columns
         self.table.columns = self.columns
-        #~ print self.table
-        #~ print self.table[self.table['PRCP'] == -9999]['PRCP_qflag']
-        warnings.filterwarnings("ignore")
-        #~ try:
-        conditions = self.config['special conditions']
-        #~ print conditions
-        for c in conditions:
-            #~ print c['condition'].keys()[0],c['condition'][c['condition'].keys()[0]]
-            #~ print self.table[c['condition'].keys()[0]]
-            #~ print c['condition'][c['condition'].keys()[0]]
-            #~ print 
-            if not c['condition'][c['condition'].keys()[0]] == "None":
-                idx = self.table[c['condition'].keys()[0]] == c['condition'][c['condition'].keys()[0]]
-            else:
-                #~ print "ELSE"
-                idx = self.table[c['condition'].keys()[0]] != c['condition'][c['condition'].keys()[0]]
-                #~ print idx
-            
-            #~ print idx
-            #~ print c['condition'].keys()[0], c['condition'][c['condition'].keys()[0]]
-            #~ print self.table[c['condition'].keys()[0]]
-            for k in c['result']:
-                #~ print c['result'][k]
-                #~ print self.table[k][idx]
-                self.table[k][idx] = c['result'][k]
-                #~ print self.table[idx][k]
-        #~ except:
-            #~ pass
-        warnings.filterwarnings("default")
+
+
+        ## Special conditions to repalce values
         
-        #~ print self.table
-        #~ print self.columns
+        if 'special conditions' in self.config.keys():
+            warnings.filterwarnings("ignore")
+            conditions = self.config['special conditions']
+            for c in conditions:
+                ## for each condition
+                
+                ## create index
+                if not c['condition'][c['condition'].keys()[0]] == "None":
+                    idx = self.table[c['condition'].keys()[0]] == \
+                        c['condition'][c['condition'].keys()[0]]
+                else:
+                    idx = self.table[c['condition'].keys()[0]] != \
+                        c['condition'][c['condition'].keys()[0]]
+        
+                ## apply resluts
+                for k in c['result']:
+                    self.table[k][idx] = c['result'][k]
+           
+            warnings.filterwarnings("default")
+        
     
     def to_string (self):
         """
@@ -280,13 +304,17 @@ class Y2I (object):
             print "An invalid time was located.'\
                     ' Please search on 'Nat' to locate"
             
-        string = '--' + self.config['sql comment'] + '\n' + string[:-2] + ';'
+        if 'sql comment' in self.config.keys():
+            string = '--' + self.config['sql comment'] +\
+                '\n' + string[:-2] + ';'
+        else: 
+            string = string[:-2] + ';'
             
         # replace null values
         return string.replace("'None'","NULL").replace("'NULL'","NULL")
 
     def save_sql (self):
-        """saves sql file as out file described in config
+        """Saves sql file as out file described in config
         """
         try:
             mode = self.config['mode']
@@ -296,17 +324,16 @@ class Y2I (object):
             sql.write(self.to_string()+'\n')
             
     def generate_sql (self):
-        """create the table and generate the sql file
+        """Create the table and generate the sql file
         """
         self.create_table()
         self.save_sql()
         
 def get_datastreamid(sitecode, varcode, creds):
-    """get datastreamid from imiq
+    """Get datastreamid from imiq
     """
     s = ph.PostHaste(*ph.load_login(creds))
-    s.sql = """
-select datastreamid from tables.datastreams 
+    s.sql = """select datastreamid from tables.datastreams 
     where 
     siteid in (select siteid from tables.sites where sitecode = 'SITECODE') 
         and 
@@ -335,10 +362,11 @@ def main ():
     config.yaml may be a yaml config file or a yaml describing a template file
     """
     try:
+        ## config is argument/template type
         setup = sys.argv[1]
         with open(setup, 'r') as s:
             setup = yaml.load(s)
-        print sys.argv[1]
+        #~ print sys.argv[1]
         for i in range(len(setup['arguments'])):
             try:
                 print setup['arguments'][i]['name']
@@ -355,10 +383,11 @@ def main ():
                 print " -- no sql generated"
                 continue
     except KeyError as e:
-        print e
+        #~ print e
+        ## config is plain config type 
         converter = Y2I(sys.argv[1])
         converter.generate_sql()
-    print "\n\n"
+    #~ print "\n\n"
     
 if __name__ == "__main__":
     main()
