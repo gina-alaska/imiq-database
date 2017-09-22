ab2e48de71c86626d11c19b17c7d7212b6410f02
Rawser Spicer <rwspicer@alaska.edu>
2017-09-22 14:11:05 -0800

    reorganized scripts dir, added new stuff

---
+++ scripts/imiq_deploy.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+# 	This Script automates the backup of the database imiq_staging and
+# and the deployment of imiq_production
+# 
+#   can be run on the database server with any user that exists there, and
+# as a user in postgres with the right permissions 
+#
+# ./deploy <path to create backup at> <#jobs, defaults to 8>
+#
+# or 
+# chage to user postgres before running
+#
+# rawser spicer
+# 2017-09-22
+#
+
+## exit on failure
+set -e 
+
+## echo lines run
+#~ set -x 
+
+## Variables
+
+
+PATH_TO_BACKUPS=$1
+if [ "$2" != "" ]; then
+    N_JOBS=$2
+else 
+   N_JOBS=8
+fi
+
+
+## Get date
+DATE=`date +%Y%m%d`
+
+## Main db names
+PROD='imiq_production'
+STAGING='imiq_staging'
+
+## Sql to rename databases
+## Replace <source> with a database, and <dest> with what to rename it 
+MOVE_DB_SQL="ALTER DATABASE <source> RENAME TO <dest>"
+
+## create backup of imiq_staging
+pg_dump -d $STAGING -j $N_JOBS -f $PATH_TO_BACKUPS$STAGING-$DATE -F d 
+
+## 	Move production temp 'backup' of production, will be dropped 
+## at end. This step is probably not necassary.
+TEMP_PROD_BACKUP="imiq_production_backup_$DATE"
+### Replaces <sourcs> and <dest>
+MOVE_TO_BACKUP="${MOVE_DB_SQL/<source>/$PROD}"
+MOVE_TO_BACKUP="${MOVE_TO_BACKUP/<dest>/$TEMP_PROD_BACKUP}"
+psql postgres -c  "$MOVE_TO_BACKUP"
+
+## Move staging to production
+STAGING_TO_PROD="${MOVE_DB_SQL/<source>/$STAGING}"
+STAGING_TO_PROD="${STAGING_TO_PROD/<dest>/$PROD}"
+psql postgres -c  "$STAGING_TO_PROD"
+
+## Drop temp 'backup' production. New production shold be in place at this point
+dropdb $TEMP_PROD_BACKUP
+
+## Restore staging
+pg_restore -d $PROD -j $N_JOBS $PATH_TO_BACKUPS$STAGING-$DATE
+
+## Ensure postgres owner ship
+psql postgres -c 'ALTER DATABASE $PROD OWNER TO postgres'
+psql postgres -c 'ALTER DATABASE $STAGING OWNER TO postgres'
---
+++ scripts/postgis-template.sql
@@ -0,0 +1,125 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: template1; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON DATABASE template1 IS 'default template for new databases';
+
+
+--
+-- Name: topology; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA topology;
+
+
+ALTER SCHEMA topology OWNER TO postgres;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: hstore; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION hstore; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION hstore IS 'data type for storing sets of (key, value) pairs';
+
+
+--
+-- Name: postgis; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION postgis IS 'PostGIS geometry, geography, and raster spatial types and functions';
+
+
+--
+-- Name: postgis_topology; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS postgis_topology WITH SCHEMA topology;
+
+
+--
+-- Name: EXTENSION postgis_topology; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION postgis_topology IS 'PostGIS topology spatial types and functions';
+
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: spatial_ref_sys; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY spatial_ref_sys  FROM stdin;
+\.
+
+
+SET search_path = topology, pg_catalog;
+
+--
+-- Data for Name: topology; Type: TABLE DATA; Schema: topology; Owner: postgres
+--
+
+COPY topology  FROM stdin;
+\.
+
+
+--
+-- Data for Name: layer; Type: TABLE DATA; Schema: topology; Owner: postgres
+--
+
+COPY layer  FROM stdin;
+\.
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
