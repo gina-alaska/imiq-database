406476c20704eae478b94d22ffbdc7506e84410b
Rawser Spicer <rwspicer@alaska.edu>
2018-07-21 10:19:25 -0800

    removed cheryls scripts

--- scripts/cheryls_scripts/crontab_postgres_imiqdb.bash
+++
@@ -1,3 +0,0 @@
-/home/postgres/tools/backup_scripts/POSTGRES/pg_dump_PARMS_DATABASE_COMPRESS.bash imiq_map_production N N Y Y
-/home/postgres/tools/backup_scripts/POSTGRES/pg_dump_PARMS_DATABASE_COMPRESS.bash imiq_staging N N Y Y 
-#/home/postgres/tools/backup_scripts/POSTGERS/pg_dump_PARMS_DATABASE_COMPRESS.bash iarcod_staging N N Y Y 
