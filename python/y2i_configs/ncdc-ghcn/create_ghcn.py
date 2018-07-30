b1120137aab8a6f6e890892c43a06ce7a2c8a9ff
Rawser Spicer <rwspicer@alaska.edu>
2018-07-30 10:32:14 -0800

    added GHCN config

---
+++ python/y2i_configs/ncdc-ghcn/create_ghcn.py
@@ -0,0 +1,40 @@
+'''
+Create GHCN Arguments
+---------------------
+File: create_ghcn_arguments.py
+
+tool for generating y2i argument files for use with the y2i ghcn conf file
+'''
+import os
+import sys
+
+
+## file name for sc_list_file, a text file with GHCN sitecodes, one on each line
+sc_list_file = sys.argv[1]
+
+## template file, use __GHCN_datavalues_args_template.yaml
+template_file = sys.argv[2]
+
+## directory to write to
+config_dir = sys.argv[3]
+
+
+with open(sc_list_file, 'r') as scl:
+    text = scl.read().strip().replace('\r','')
+    sc_list = text.split('\n')
+
+for site in sc_list:
+    print site
+    with open(template_file, 'r') as arg_temp:
+        for i in range(6):
+            arg_temp.readline()
+        text = arg_temp.read().replace("<SITECODE>", site)
+
+    comment = "#Y2I argument file for GHCN site " + site + "\n"
+
+    text = comment + text
+
+    filename = site + '.yaml'
+    cfg = os.path.join(config_dir, filename)
+    with open(cfg, 'w') as cfg_f:
+        cfg_f.write(text)
